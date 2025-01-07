e00aea2a33f0962c0cf246b914d647789f115f42
Vincenzo Chiovaro <vincenzochiovaro194@gmail.com>
2025-01-07 06:02:05 +0000

    Add CockroachDB connection class

--- Data/CockroachDb.cs
+++ Data/CockroachDb.cs
@@ -1,9 +1,24 @@
+using Npgsql;
+
 namespace SeasonalBite.Data;
 
-public class CockroachDB
+public class CockroachDb
 {
-    void GetConnection()
+    private string _connectionString;
+
+    public CockroachDb()
     {
-        
+        _connectionString = Environment.GetEnvironmentVariable("COCKROACH_CONN_STR");
+
+        if (string.IsNullOrEmpty(_connectionString))
+        {
+            throw new Exception("Connection string not found.");
+        }
+    }
+    
+    public NpgsqlConnection GetConnection()
+    {
+        var connection = new NpgsqlConnection(_connectionString);
+        return connection;
     }
 }
