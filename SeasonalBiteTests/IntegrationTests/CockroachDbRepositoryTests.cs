8ba82341d2a4716461edcbbafb13b62b7250bc22
Vincenzo Chiovaro <66358836+vincenzochiovaro@users.noreply.github.com>
2025-01-24 05:14:03 +0000

    Add integration test for GetAlimentsAsync (#9)

---
+++ SeasonalBiteTests/IntegrationTests/CockroachDbRepositoryTests.cs
@@ -0,0 +1,30 @@
+using FluentAssertions;
+using SeasonalBite.Data;
+using SeasonalBite.Interfaces;
+using SeasonalBite.Models;
+using SeasonalBite.Services;
+
+namespace SeasonalBiteUnitTests;
+
+public class CockroachDbRepositoryTests
+{
+    private readonly IDbManager _db; 
+    private readonly CockroachDbRepository _repository;
+    public CockroachDbRepositoryTests()
+    {
+        _db = new CockroachDb();
+        _repository = new CockroachDbRepository(_db);
+    }
+    [Fact]
+    public async void GivenValidDatabaseConn_WhenGetAlimentCalled_ThenReturnAlimentList()
+    {
+        // When
+        var result = await _repository.GetAlimentsAsync();
+        
+        // Then
+        var firstAliment = result.First();
+        firstAliment.Should().NotBeNull();
+        result.Should().BeOfType<List<Aliment>>();
+        result.Should().NotBeEmpty();
+    }
+}
