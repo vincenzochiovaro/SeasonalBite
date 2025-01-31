3917e53a11363c9ab3c43d5f2b48b13a2f901609
Vincenzo Chiovaro <66358836+vincenzochiovaro@users.noreply.github.com>
2025-01-31 20:04:15 +0000

    resolve failed to read PNG signature error by using proper PNG files (#12)
    * resolve failed to read PNG signature error by using proper PNG files
    
    * _

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -2,8 +2,12 @@
 
 public partial class AppShell : Shell
 {
+    public string ShellTitle { get; set; }
+
     public AppShell()
     {
         InitializeComponent();
+        ShellTitle = $"SeasonalBite - {DateTime.Now:MMMM}";
+        BindingContext = this;
     }
 }
