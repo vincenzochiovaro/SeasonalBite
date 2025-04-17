b9f4e8c9fcf978736da94ee02bca627823b5b07b
Vincenzo Chiovaro <66358836+vincenzochiovaro@users.noreply.github.com>
2025-04-17 06:11:44 +0100

    Add app icon (#23)
    * Add app icon
    
    * Add app icon

--- Models/Aliment.cs
+++ Models/Aliment.cs
@@ -3,5 +3,5 @@ namespace SeasonalBite.Models;
 public class Aliment
 {
     public required string Name { get; set; }
-    public string ImageUrl => $"https://raw.githubusercontent.com/vincenzochiovaro/seasonalBite-assets/refs/heads/main/images/{Name.ToLower().Replace(" ", "")}.png";
+    public string ImageUrl => $"https://raw.githubusercontent.com/vincenzochiovaro/SeasonalBite-assets/refs/heads/main/images/{Name.ToLower().Replace(" ", "")}.png";
 }
